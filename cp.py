69e14d29a24a4011d5774e2f422dda30e8ccb7aa
siddhath0595 <siddharthchoudhary111@gmail.com>
2022-12-17 19:24:07 +0530

    v1 of python

---
+++ cp.py
@@ -0,0 +1,14 @@
+try:
+	def cp():
+		global b
+		with open(r"/home/ubuntu/f1.txt","w") as i:
+			i.write("hello this is my jenkins pipeline")
+		with open(r"/home/ubuntu/f1.txt","r") as i:
+			b=i.read()
+		with open(r"/home/ubuntu/file2.txt","x") as i:
+			i.write(b)
+	cp()
+except FileExistsError as i:
+	with open(r"/home/ubuntu/file2.txt","a") as i:
+		i.write(b)
+		#print("file data has been copy successfully by append mode")
